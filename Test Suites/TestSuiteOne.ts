3e215e63c616e7a3241fc4f661d06d3ab2cefc43
faiqdhimas <faiq.dhms@gmail.com>
2021-09-02 10:29:27 +0700

    dari jenkins (katalon integration with jenkins+git)

--- Test Suites/TestSuiteOne.ts
+++ Test Suites/TestSuiteOne.ts
@@ -4,11 +4,23 @@
    <name>TestSuiteOne</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <mailRecipient>profosaid@gmail.com;</mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>aeb9f2f2-985f-47e1-a126-e7d8867357a6</testSuiteGuid>
+   <testCaseLink>
+      <guid>f68107a0-00c4-47fb-8ef2-f6b3e2eb2aac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test 1 testerrr uy</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8dfe9a48-45db-45a6-aa12-070fe9eb467e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test 2</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
