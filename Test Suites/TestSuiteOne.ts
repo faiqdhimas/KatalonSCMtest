a60cbf3ae89ef50ec40693214c4ec21c7f271bf5
faiqdhimas <faiq.dhms@gmail.com>
2021-09-02 10:21:15 +0700

    dari jenkins (katalon integration with jenkins+git)

---
+++ Test Suites/TestSuiteOne.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuiteOne</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>aeb9f2f2-985f-47e1-a126-e7d8867357a6</testSuiteGuid>
+</TestSuiteEntity>
